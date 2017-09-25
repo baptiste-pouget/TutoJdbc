1ae9a0f033876b05996d78ac2fc2a3a9f207103e
Baptiste <baptiste.pouget@etu.univ-amu.fr>
2017-09-25 10:39:24 +0200

    quomite

--- src/main/java/fr/univ_amu/iut/TestJDBC.java
+++ src/main/java/fr/univ_amu/iut/TestJDBC.java
@@ -5,9 +5,9 @@ import java.sql.*;
 
 public class TestJDBC {
 	// Chaine de connexion
-	static final String CONNECT_URL = "jdbc:mysql://localhost:3306/gestionPedaBD";
-	static final String LOGIN = "monUser";
-	static final String PASSWORD = "monPassword";
+	static final String CONNECT_URL = "jdbc:mysql://mysql-baptiste-pouget.alwaysdata.net:3306/baptiste-pouget_db";
+	static final String LOGIN = "144386";
+	static final String PASSWORD = "mdp";
 	// La requete de test
 	static final String req = "SELECT NUM_ET, NOM_ET, PRENOM_ET " +
 	                          "FROM ETUDIANT " +
